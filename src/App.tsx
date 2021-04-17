508db5a49255de6610c82c7981bd0b70af2e8733
Prakash <prakash106kumar@gmail.com>
2021-04-17 12:11:24 +0530

    refactor(app): delete unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import { PatternView, Settings } from './components'
 
-import './App.css'
-
-function App() {
-
+const App: React.FC = () => {
   return (
     <div className="grid grid-cols-3 p-8 gap-8">
       <Settings />
